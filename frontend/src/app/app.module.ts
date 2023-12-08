1ad21af80e402364c2d8a01ad24812042d38676f
IKala4I <vpersko@gmail.com>
2023-12-08 14:19:15 +0200

    + create frontend part

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/partials/header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HomeComponent} from './components/pages/home/home.component';
+import {SearchComponent} from './components/partials/search/search.component';
+import {FoodPageComponent} from './components/pages/food-page/food-page.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    FoodPageComponent,
+    TagsComponent,
+    CartPageComponent,
+    TitleComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
