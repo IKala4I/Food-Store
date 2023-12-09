0f1961ca8b6d3a808194b6b1ad6a1718ac5effe8
IKala4I <vpersko@gmail.com>
2023-12-10 00:18:06 +0200

    + update order router

--- backend/src/routers/order.router.ts
+++ backend/src/routers/order.router.ts
@@ -33,7 +33,7 @@ router.post('/create', asyncHandler(
 
 router.get('/newOrderForCurrentUser', asyncHandler(
   async (req: any, res) => {
-    const order = await OrderModel.findOne({user: req.user.id, status: OrderStatus.NEW});
+    const order = await getNewOrderForCurrentUser(req);
 
     if (order)
       res.send(order);
@@ -41,5 +41,27 @@ router.get('/newOrderForCurrentUser', asyncHandler(
       res.status(HTTP_BAD_REQUEST).send();
   }
 ));
+
+router.post('/pay', asyncHandler(
+  async (req: any, res) => {
+    const {paymentId} = req.body;
+    const order = await getNewOrderForCurrentUser(req);
+
+    if (!order) {
+      res.status(HTTP_BAD_REQUEST).send('Order Not Found!');
+      return;
+    }
+
+    order.paymentId = paymentId;
+    order.status = OrderStatus.PAYED;
+    await order.save();
+
+    res.send(order._id);
+  }
+));
+
+async function getNewOrderForCurrentUser(req: any) {
+  return OrderModel.findOne({user: req.user.id, status: OrderStatus.NEW});
+}
 
 export default router;
