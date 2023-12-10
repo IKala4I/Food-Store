ebfc2f11daaa0bf540e0e451daa67a0c7c930edf
IKala4I <vpersko@gmail.com>
2023-12-10 10:32:42 +0200

    * update urls

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,4 +1,10 @@
-const BASE_URL = `http://localhost:10000`;
+import {environment} from '../../../environments/environment';
+
+let BASE_URL = `http://localhost:10000`;
+
+if (environment.production) {
+  BASE_URL = 'https://foodstore-dw8c.onrender.com';
+}
 export const FOODS_URL = BASE_URL + '/api/foods';
 export const FOODS_TAGS_URL = FOODS_URL + '/tags';
 export const FOODS_BY_SEARCH_URL = FOODS_URL + '/search/';
